62454245e9feb9b0b9a5a3a8cebd0f98441900a4
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-04-15 18:14:01 -0400

    finished seed file

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -6,5 +6,5 @@ exports.seed = function(knex) {
     mode: ' truncate', //resets ids
     ignoreTables: ['knex_migrations',
      'knex_migrations_lock'] //don't empty migration tables
-  })
+  });
 };
